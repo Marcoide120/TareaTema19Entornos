fb9c6424fdc7961abdf025bb5404bf2e14e63043
Marcoide120 <marcosgarciacasta5000@gmail.com>
2024-04-29 18:41:43 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo");
+
+        //Suma
+
+        int numero1 = 2;
+        int numero2 = 4;
+
+        int total = numero1  + numero2;
+
+        System.out.println("Total: " + total);
+
+    }
+}
